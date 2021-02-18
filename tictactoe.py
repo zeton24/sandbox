5684e4fb6e9cf587c5c407e38f4c4a389bd98767
zeton24 <zofia_jethon@hotmail.com>
2021-02-18 10:35:03 +0100

    Enables game with NxN board

--- tictactoe.py
+++ tictactoe.py
@@ -8,51 +8,54 @@ class Settings(BaseModel):
 
 
 class TicTacToe():
-        self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
-        self.whose_turn = np.random.choice(['X','O']) 
-        self.free_fields = list(range(9))
-        self.winning_sets = [{0,1,2},{3,4,5},{6,7,8},
-                            {0,3,6},{1,4,7},{2,5,8},
-                            {0,4,8},{6,4,2}]
     def __init__(self, settings):
         self.players = settings.number_of_players
+        self.size = settings.size
+        self.how_many_to_win = 3
+        self.board = self.create_board(settings.size)
+        self.whose_turn = np.random.choice(['X','O'])
+        self.free_fields = list(range(settings.size**2))
         self.moves =  {'X': set(), 'O': set()}
 
 
     def how_to_play(self):
-        print('How to play: choose a number of field where you want to put a mark.\n')
-        print(" 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9\n")
+        print('How to play: choose a number of field where you want to put a mark.\n' \
+             'Fields are numerate from left to right and top-bottom starting with 1.\n')
         print("Let's begin!")
-    
+
+    def create_board(self,size):
+        board = np.chararray(shape=(size,size), unicode=True)
+        return board
 
     def make_move(self, player, auto=False) -> None:
         if auto:
             field_number = np.random.choice(self.free_fields)
         else:
-            field_number = input(f'{player} player moves: ')
-            field_number = int(field_number) - 1
+            field_number = int(input(f'{player} player moves: ')) - 1
 
         if field_number not in self.free_fields:
-            print("You can't make this move.")
-            self.make_move(player)
+                print("You can't make this move.")
+                self.make_move(player)
         else:
-            x, y = np.divmod(field_number, 3)
-            self.board[x][y] = player
+            x, y = np.divmod(field_number, self.size)
+            self.board[x,y] = player
             self.free_fields.remove(field_number)
             self.moves[self.whose_turn].add(field_number)
 
-
     def check_winner(self) -> str:
-        result = None
-        for l in self.winning_sets:
-            if l.issubset(self.moves[self.whose_turn]):
-                result = self.whose_turn
-                break
-        return result
+        for i,field in enumerate(self.moves[self.whose_turn]):
+            row_win = set([field + x for x in range(self.how_many_to_win)])
+            column_win = set([field + self.size*x for x in range(self.how_many_to_win)])
+            diagonal_win = set([field + self.size*x for x in range(self.how_many_to_win)])
+            if (row_win & self.moves[self.whose_turn] ==  row_win) \
+                or (column_win & self.moves[self.whose_turn] ==  column_win) \
+                or (diagonal_win & self.moves[self.whose_turn] ==  diagonal_win):
+                return self.whose_turn
 
 
     def print_board(self) -> None:
-        rows = []
-        for x in self.board:
-            rows.append(f"\n {x[0]} | {x[1]} | {x[2]}\n")
-        print(('-' *11).join(rows))
+        print()
+        for row in self.board[:-1, :]:
+            print('  |  '.join(row))
+            print('_' *int(np.ceil(4*self.size)))
+        print('  |  '.join(self.board[-1,:]))
